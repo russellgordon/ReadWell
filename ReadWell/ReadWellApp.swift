00c37e19aeb8333ef5c0df130e1683ba272470e1
Russell Gordon <rgordon@lcs.on.ca>
2024-06-05 20:10:45 -0400

    Initial Commit

---
+++ ReadWell/ReadWellApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadWellApp.swift
+//  ReadWell
+//
+//  Created by Russell Gordon on 2024-06-05.
+//
+
+import SwiftUI
+
+@main
+struct ReadWellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
