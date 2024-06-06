163a38da4f71ce8c93d5873d5e4fd05009b3aa4c
Russell Gordon <rgordon@lcs.on.ca>
2024-06-05 20:20:08 -0400

    Did initial project setup.

--- ReadWell/ReadWellApp.swift
+++ ReadWell/ReadWellApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ReadWellApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SplashScreenView()
         }
     }
 }
---
+++ ReadWell/Views/SplashScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreenView.swift
+//  ReadWell
+//
+//  Created by Russell Gordon on 2024-06-05.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}
