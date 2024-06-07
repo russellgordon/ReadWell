f936882d808503cda409e010c11daf4be928aef7
Russell Gordon <rgordon@lcs.on.ca>
2024-06-06 20:38:16 -0400

    Simplified the reviews list view by removing the segmented picker that previously allowed us to change the way the reviewed books were presented.

--- ReadWell/Views/ReviewsListView.swift
+++ ReadWell/Views/ReviewsListView.swift
@@ -10,47 +10,24 @@ import SwiftUI
 struct ReviewsListView: View {
     
     // MARK: Stored properties
-    @State private var selectedPresentationType: Int = 1
-    
     @State private var addNewReviewSheetIsShowing = false
     
     // MARK: Computed properties
     var body: some View {
         NavigationStack {
-            VStack {
-                
-                Picker("Presenting by...", selection: $selectedPresentationType) {
-                    Text("By Title").tag(1)
-                    Text("By Author").tag(2)
-                    Text("By Genre").tag(3)
-                }
-                .pickerStyle(.segmented)
-                .padding()
-                
-                List {
-                    
-                    if selectedPresentationType == 1 {
-                        Text("Dune")
-                        Text("Outlander")
-                        Text("Pride and Prejudice")
-                    } else if selectedPresentationType == 2 {
-                        Text("Austen, Jane")
-                        Text("Gabaldon, Diana")
-                        Text("Herbert, Frank")
-                    } else {
-                        Text("Romance")
-                        Text("Science Fiction")
-                    }
-                    
-                }
-                .listStyle(.plain)
-                .background {
-                    Color.readWellBrown
-                        .ignoresSafeArea()
-                }
-                .searchable(text: Binding.constant(""))
-                
+            List {
+                Text("Dune")
+                Text("Outlander")
+                Text("Pride and Prejudice")
             }
+            .listStyle(.plain)
+            .background {
+                Color.readWellBrown
+                    .ignoresSafeArea()
+            }
+            .toolbarBackground(Color.tabBar, for: .navigationBar)
+            .toolbarBackground(.visible, for: .navigationBar)
+            .searchable(text: Binding.constant(""))
             .toolbar {
                 
                 ToolbarItem(placement: .topBarLeading) {
